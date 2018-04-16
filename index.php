279c74680cbf5ada25cd18c07398e5acb5a3cb53
Stef <oljacici@gmail.com>
2018-04-16 11:46:50 +0200

    All you need about pdo essential is here(example without OOP).

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+
+if (get_magic_quotes_gpc()) {
+    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+
+    while (list($key, $val) = each($process)) {
+        foreach ($val as $k => $v) {
+            unset($process[$key][$k]);
+            if (is_array($v)) {
+                $process[$key][stripslashes($k)] = $v;
+                $process[] = &$process[$key][stripcslashes($k)];
+            }
+            else {
+                $process[$key][stripslashes($k)] = stripcslashes($v);
+            }
+        }
+    }
+    unset($process);
+}
+
+if (isset($_GET['addjoke'])) {
+    include 'template/form.html.php';
+    exit();
+}
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=ijdb', 'ijdbuser', 'mypass');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec('SET NAMES "utf8"');
+}
+catch (PDOException $e) {
+    $error = 'Unable to connect to the database server.';
+    include 'template/error.html.php';
+    exit();
+}
+
+if (isset($_POST['joketext'])) {
+    try {
+        $sql = 'INSERT INTO joke SET
+            joketext = :joketext,
+            jokedate = CURDATE()';
+        $s = $pdo->prepare($sql);
+        $s->bindValue(':joketext', $_POST['joketext']);
+        $s->execute();
+    }
+    catch (PDOException $e) {
+        $error = 'Error adding submited joke:' . $e->getMessage();
+        include 'template/error.html.php';
+        exit();
+    }
+
+    header('Location: .');
+    exit();
+}
+
+if (isset($_GET['deletejoke']))
+{
+    try {
+        $sql = 'DELETE FROM joke WHERE id = :id';
+        $s = $pdo->prepare($sql);
+        $s->bindValue(':id', $_POST['id']);
+        $s->execute();
+    }
+    catch(PDOException $e) {
+        $error = 'Error deleting joke: ' . $e->getMessage();
+        include 'template/error.html.php';
+        exit();
+    }
+}
+
+try {
+    $sql = 'SELECT id, joketext FROM joke';
+    $result = $pdo->query($sql);
+}
+catch(PDOException $e)  {
+    $error = 'Error fetching jokes: ' . $e->getMessage();
+    include 'template/error.html.php';
+    exit();
+}
+// while loope example in list jokes folder
+foreach ($result as $row) {
+    $jokes[] = array('id' => $row['id'], 'text' => $row['joketext']);
+}
+
+include 'template/jokes.html.php';
---
+++ template/form.html.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        textarea {
+            display: block;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form action="?" method="post">
+        <div>
+            <label for="joketext">Type your joke here:</label>
+            <textarea name="joketext" id="joketext" cols="40" rows="3"></textarea>
+        </div>
+        <div><input type="submit" value="Add"></div>
+    </form>
+</body>
+</html>
